a2016319ec546e5f0a90c2767f46f7a4a6db4e77
Caspar Boetes <caspar@puukboek.local>
2019-12-01 15:38:09 +0100

    update secrets files ;)

--- src/secrets.js
+++ src/secrets.js
@@ -5,29 +5,3 @@ const secret = {
 };
 
 export default secret;
-
-// <img
-//  class="game-image"
-//  src="https://api-test.newzoo.com:443/v1.0/metadata/game/boxart?name=League of Legends"
-//  alt="cover League of Legends"
-//  ></img>
-
-// width 230.281 x height 321.703
-
-// detailpage
-// :authority: api.newzoo.com
-// :method: GET
-// :path: /v1.0/metadata/game/c6b51f05-d692-40f8-86dc-1ac5851581d8?__permission_set=Explorer%20Games
-
-// ROUTES FUCKUO WITH REDUX
-// // before
-// export default connect(mapStateToProps)(Something)
-
-// // after
-// import { withRouter } from 'react-router-dom'
-// export default withRouter(connect(mapStateToProps)(Something))
-// Deep integration
-//
-// SASSS SHIT
-// @import 'styles/_colors.scss'; // assuming a styles directory under src/
-// @import '~nprogress/nprogress'; // importing a css file from the nprogress node module
