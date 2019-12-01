5d9ddb8701461ef80cc8ba0d15b1971d4f2af0b3
Caspar Boetes <caspar@puukboek.local>
2019-12-01 15:34:38 +0100

    refactor api

--- src/api/newzoo.js
+++ src/api/newzoo.js
@@ -9,33 +9,13 @@ const api = (endpoint, { method, data, jwt } = {}) => {
       Authorization: `Bearer ${jwt}`
     }
   })
-    .then(function(response) {
+    .then(response => {
       const { data } = response;
       return data;
     })
-    .catch(function(error) {
+    .catch(error => {
       return console.log("oopsie, error");
     });
 };
-
-// function api(endpoint, { method, body, jwt } = {}) {
-//   return fetch("https://api.newzoo.com/v1.0/" + endpoint, {
-//     method: method,
-//     headers: {
-//       Authorization: `Bearer ${jwt}`
-//     },
-//     body: JSON.stringify(body)
-//   })
-//     .then(response => Promise.all([response.status, response.json()]))
-//     .then(([status, data]) => {
-//       if (status >= 400) {
-//         // throw { api_error: data };
-//         console.log("Oopsie, error");
-//       } else {
-//         console.log("data in call itself", data);
-//         return data;
-//       }
-//     });
-// }
 
 export default api;
