5ca8da35992422ee7e589daf452ddb86d3101af5
Caspar Boetes <caspar@puukboek.local>
2019-12-14 07:35:51 +0100

    refactor to handle hooks

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,24 +3,22 @@ import { Link } from 'react-router-dom'
 import SearchBar from './SearchBar'
 import './NavBar.scss'
 
-class NavBar extends React.Component {
-  render() {
-    return (
-      <nav className='nav-bar'>
-        <div className='container'>
-          <Link to={'/'} className='logo'>
-            <img
-              src={
-                'https://newzoo.com/wp-content/themes/newzoo/images/newzoo-logo.svg'
-              }
-              alt='Newzoo Logo'
-            ></img>
-          </Link>
-          <SearchBar />
-        </div>
-      </nav>
-    )
-  }
+const NavBar = () => {
+  return (
+    <nav className='nav-bar'>
+      <div className='container'>
+        <Link to={'/'} className='logo'>
+          <img
+            src={
+              'https://newzoo.com/wp-content/themes/newzoo/images/newzoo-logo.svg'
+            }
+            alt='Newzoo Logo'
+          ></img>
+        </Link>
+        <SearchBar />
+      </div>
+    </nav>
+  )
 }
 
 export default NavBar
